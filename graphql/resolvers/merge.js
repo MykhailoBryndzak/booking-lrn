eea87764269426e01d3ecd222511aab8d682c1e8
Bryndzak Mykhailo <mykhailo.bryndzak@gmail.com>
2019-02-04 17:21:11 +0200

    added dataLoader for optimizing database requests

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,15 @@
 const User = require('../../models/user');
 const Event = require('../../models/event');
 const {dateToString} = require('../../helpers/date');
+const DataLoaser = require('dataloader');
+
+const eventLoader = new DataLoaser((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoaser((userIds) => {
+  return User.find({_id: {$in: userIds}});
+});
 
 const transformBooking = booking => {
   return {
@@ -26,7 +35,7 @@ const events = async eventIds => {
   try {
     const events = await Event.find({_id: {$in: eventIds}});
     return events.map(event => {
-      return transformEvent(event);
+      return event;
     })
   } catch (err) {
     throw err
@@ -35,7 +44,7 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
   try {
-    const event = await Event.findById({_id: eventId});
+    const event = await eventLoader.load(eventId.toString());
     return transformEvent(event)
   } catch (err) {
     throw err
@@ -44,11 +53,11 @@ const singleEvent = async eventId => {
 
 const user = async userId => {
   try {
-    const user = await User.findById(userId)
+    const user = await userLoader.load(userId.toString())
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents)
     }
   } catch (err) {
     throw err
